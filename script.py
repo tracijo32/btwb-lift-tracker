98cd8a8d9ef63a336b32adfce07aa8cc2d985fc6
Traci Johnson <traci.johnson.32@gmail.com>
2026-03-14 21:21:59 -0500

    added download from , upload to GCS

--- script.py
+++ script.py
@@ -4,23 +4,36 @@ from plotting import (
     create_squat_strength_figure,
     create_withings_figure,
 )
+from helpers import save_plotly_figure
 
 withings_data, withings_model = process_withings_data('gs://withings-data')
 squat_data, squat_model = process_squats('gs://btwb-workouts')
 
-create_bwbs_forecast_figure(
-    withings_data, 
-    squat_data,
-    withings_model,
-    squat_model
-).write_html('bwbs_forecast.html')
+save_plotly_figure(
+    create_bwbs_forecast_figure(
+        withings_data, 
+        squat_data, 
+        withings_model, 
+        squat_model
+    ), 
+    "gs://box.crossvalidatedfitness.com/figures/bwbs_forecast.html",
+    project="crossvalidatedfitness-prod"
+)
 
-create_squat_strength_figure(
-    squat_data, 
-    squat_model
-).write_html('squat_strength.html')
+save_plotly_figure(
+    create_squat_strength_figure(
+        squat_data, 
+        squat_model
+    ), 
+    "gs://box.crossvalidatedfitness.com/figures/squat_strength.html",
+    project="crossvalidatedfitness-prod"
+)
 
-create_withings_figure(
-    withings_data,
-    withings_model
-).write_html('withings.html')
+save_plotly_figure(
+    create_withings_figure(
+        withings_data, 
+        withings_model
+    ), 
+    "gs://box.crossvalidatedfitness.com/figures/withings.html",
+    project="crossvalidatedfitness-prod"
+)
